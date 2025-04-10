c64f78cd63ae7a548e1ad19fcfb2446718d1ee1b
Atanu-hub <atanudatta30@gmail.com>
2025-04-10 23:09:36 +0530

    titke mof

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 error_reporting(0);
 include("connection.php");
 session_start();
-// include("checked-login.php");
+include("checked-login.php");
 ?>
 
 
@@ -11,7 +11,7 @@ session_start();
 
 <head>
   <?php include("./components/headlink.php"); ?>
-  <title>Usemee - eCommerce Website</title>
+  <title>Usemee___ eCommerce Website</title>
 
 
 </head>
